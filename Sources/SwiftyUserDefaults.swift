57225d8c3655bcd7bca5ee9cb374c4764699d1d6
Maxim Ananov <pointum@gmail.com>
2016-07-12 21:37:26 +0300

    Added optional default value to DefaultsKey

--- Sources/SwiftyUserDefaults.swift
+++ Sources/SwiftyUserDefaults.swift
@@ -189,14 +189,19 @@ public class DefaultsKeys {
 }
 
 /// Base class for static user defaults keys. Specialize with value type
-/// and pass key name to the initializer to create a key.
+/// and pass `key` name to the initializer to create a key.
+/// Optionally a `defaultValue` can be used as a fallback for this key.
 
 public class DefaultsKey<ValueType>: DefaultsKeys {
     // TODO: Can we use protocols to ensure ValueType is a compatible type?
     public let _key: String
     
-    public init(_ key: String) {
+    public init(_ key: String, _ defaultValue: ValueType? = nil) {
         self._key = key
+
+        if let object = defaultValue as? AnyObject {
+            Defaults.registerDefaults([key: object])
+        }
     }
 }
 
