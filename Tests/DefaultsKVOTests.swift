21fe992f13ae6d523cc555b814b64a837fc49ad6
Christian Tietze <christian.tietze@gmail.com>
2016-11-26 11:36:33 +0100

    import & adapt tests from github.com/toshi0383

---
+++ Tests/DefaultsKVOTests.swift
@@ -0,0 +1,75 @@
+import XCTest
+@testable import SwiftyUserDefaults
+
+class DefaultsKVOTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        clearDefaults()
+    }
+
+    func testKVO_ReceivesChanges() {
+        Defaults["count"] = 0
+        let ex = expectation(description: "`count` should be increased.")
+        let disposable = Defaults.observe(key: "count") { proxy in
+            if let int = proxy.int, int == 1 {
+                ex.fulfill()
+            } else {
+                XCTFail()
+            }
+        }
+        Defaults["count"] = 1
+        waitForExpectations(timeout: 0.1, handler: nil)
+        disposable.dispose()
+        Defaults.remove("count")
+    }
+
+    func testKVO_DisposingRemovesObserver() {
+        Defaults["count"] = 0
+        let disposable = Defaults.observe(key: "count") { proxy in
+            XCTFail()
+        }
+        disposable.dispose()
+        Defaults["count"] = 1
+        XCTAssert(disposable.handler == nil)
+    }
+
+    func testKVO_DisposingRemovesHandler() {
+        Defaults["count"] = 0
+        let disposable = Defaults.observe(key: "count") { proxy in
+            XCTFail()
+        }
+        let ex = expectation(description: "`count` should be increased.")
+        let disposable2 = Defaults.observe(key: "count") { proxy in
+            if let int = proxy.int, int == 1 {
+                ex.fulfill()
+            } else {
+                XCTFail()
+            }
+        }
+        disposable.dispose()
+        Defaults["count"] = 1
+        XCTAssert(disposable.handler == nil)
+        XCTAssert(disposable2.handler != nil)
+        waitForExpectations(timeout: 0.1, handler: nil)
+        disposable2.dispose()
+
+        XCTAssert(disposable2.handler == nil)
+    }
+
+    func testKVO_WithPlainFoundation() {
+        UserDefaults.standard.set(0, forKey: "count")
+        let ex = expectation(description: "`count` should be increased.")
+        let disposable = Defaults.observe(key: "count") { proxy in
+
+            if let int = proxy.int, int == 1 {
+                ex.fulfill()
+            } else {
+                XCTFail()
+            }
+        }
+        UserDefaults.standard.set(1, forKey: "count")
+        waitForExpectations(timeout: 0.1, handler: nil)
+        disposable.dispose()
+    }
+}
