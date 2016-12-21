425afa8fbcbae8734a60918d25d52f50c5f11bf0
David Whetstone <david@humblehacker.com>
2016-12-21 14:03:50 -0800

    Make DefaultsKey open for subclassing

--- Sources/SwiftyUserDefaults.swift
+++ Sources/SwiftyUserDefaults.swift
@@ -190,14 +190,14 @@ public let Defaults = UserDefaults.standard
 /// Extend this class and add your user defaults keys as static constants
 /// so you can use the shortcut dot notation (e.g. `Defaults[.yourKey]`)
 
-public class DefaultsKeys {
+open class DefaultsKeys {
     fileprivate init() {}
 }
 
 /// Base class for static user defaults keys. Specialize with value type
 /// and pass key name to the initializer to create a key.
 
-public class DefaultsKey<ValueType>: DefaultsKeys {
+open class DefaultsKey<ValueType>: DefaultsKeys {
     // TODO: Can we use protocols to ensure ValueType is a compatible type?
     public let _key: String
     
