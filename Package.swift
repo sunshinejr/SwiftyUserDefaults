ec4dddd1e9656efe9c4a7bc001f937eba8b9c90d
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2018-02-18 00:34:04 +0100

    SPM upgrade to PackageDescriptionV4

--- Package.swift
+++ Package.swift
@@ -1,8 +1,23 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "SwiftyUserDefaults",
+    products: [
+        .library(
+            name: "SwiftyUserDefaults",
+            targets: ["SwiftyUserDefaults"]),
+    ],
     dependencies: [],
-    exclude: ["Sources/Info.plist", "Sources/SwiftyUserDefaults.h"]
+    targets: [
+        .target(
+            name: "SwiftyUserDefaults",
+            dependencies: [],
+            path: "Sources"),
+        .testTarget(
+            name: "SwiftyUserDefaultsTests",
+            dependencies: ["SwiftyUserDefaults"]),
+    ]
 )
-
