d9bca6b2097111fb30a832af182c5e740db3a1c7
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2018-05-06 00:48:26 +0200

    FrogDefaultSerializableSpecs

--- Tests/SwiftyUserDefaultsTests/TestHelper.swift
+++ Tests/SwiftyUserDefaultsTests/TestHelper.swift
@@ -32,6 +32,18 @@ struct FrogCodable: Codable {
     }
 }
 
+struct FrogDefaultCodable: Codable, DefaultsDefaultValueType, DefaultsDefaultArrayValueType {
+
+    static let defaultValue: FrogDefaultCodable = FrogDefaultCodable(name: "frog default")
+    static let defaultArrayValue: [FrogDefaultCodable] = []
+
+    let name: String
+
+    init(name: String = "Froggy") {
+        self.name = name
+    }
+}
+
 final class FrogSerializable: NSObject, NSCoding, DefaultsSerializable {
 
     let name: String
@@ -52,6 +64,34 @@ final class FrogSerializable: NSObject, NSCoding, DefaultsSerializable {
 
     override func isEqual(_ object: Any?) -> Bool {
         guard let rhs = object as? FrogSerializable else { return false }
+
+        return name == rhs.name
+    }
+}
+
+final class FrogDefaultSerializable: NSObject, NSCoding, DefaultsSerializable, DefaultsDefaultValueType, DefaultsDefaultArrayValueType {
+
+    static let defaultValue: FrogDefaultSerializable = FrogDefaultSerializable(name: "frog default")
+    static let defaultArrayValue: [FrogDefaultSerializable] = []
+
+    let name: String
+
+    init(name: String = "Froggy") {
+        self.name = name
+    }
+
+    init?(coder aDecoder: NSCoder) {
+        guard let name = aDecoder.decodeObject(forKey: "name") as? String else { return nil }
+
+        self.name = name
+    }
+
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+    }
+
+    override func isEqual(_ object: Any?) -> Bool {
+        guard let rhs = object as? FrogDefaultSerializable else { return false }
 
         return name == rhs.name
     }
