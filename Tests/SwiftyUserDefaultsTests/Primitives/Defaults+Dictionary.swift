d8bef2b5a2e23722e4cf149548da1d6c3a11793f
Łukasz Mróz <thesunshinejr@gmail.com>
2019-02-21 14:56:15 +0100

    Update license year

--- Tests/SwiftyUserDefaultsTests/Primitives/Defaults+Dictionary.swift
+++ Tests/SwiftyUserDefaultsTests/Primitives/Defaults+Dictionary.swift
@@ -1,7 +1,7 @@
 //
 // SwiftyUserDefaults
 //
-// Copyright (c) 2015-2018 Radosław Pietruszewski, Łukasz Mróz
+// Copyright (c) 2015-present Radosław Pietruszewski, Łukasz Mróz
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
