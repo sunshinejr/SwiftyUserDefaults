816340c9eee02ee3387ffd208d4649876e7c22d7
Łukasz Mróz <thesunshinejr@gmail.com>
2019-08-12 15:25:29 +0200

    Add support for Swift 4.2 libs in SPM

--- Package_4.2.swift
+++ Package_4.2.swift
@@ -11,8 +11,8 @@ let package = Package(
             targets: ["SwiftyUserDefaults"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/sunshinejr/Quick.git", .branch("feature/update_nimble")),
-        .package(url: "https://github.com/Quick/Nimble.git", .branch("master"))
+        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "1.3.0")),
+        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "7.1.0"))
     ],
     targets: [
         .target(
