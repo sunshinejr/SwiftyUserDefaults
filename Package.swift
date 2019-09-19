1656e6d0a2a53cc75a677df0fa03e7a57bf2868f
Łukasz Mróz <thesunshinejr@gmail.com>
2019-09-19 18:09:28 +0200

    Fix parsing the SPM manifest

--- Package.swift
+++ Package.swift
@@ -34,5 +34,5 @@ let package = Package(
             targets: ["SwiftyUserDefaults"]),
     ],
     dependencies: resolveDependencies(),
-    targets: resolveTargets(),
+    targets: resolveTargets()
 )
